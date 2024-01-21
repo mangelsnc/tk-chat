c97435cbcc68a6083b6f445c7f5a20d53963fff9
Miguel Angel Sanchez Chordi <mangel.snc@gmail.com>
2024-01-21 17:52:08 +0100

    Refactor sever to OOP

--- server.py
+++ server.py
@@ -3,53 +3,57 @@
 import socket
 import threading
 
-clients = []
-usernames = {}
+class Server:
 
-SERVER_HOST = 'localhost'
-SERVER_PORT = 12345
+    def __init__(self, port: int = 12345) -> None:
+    
+        self.clients = []
+        self.usernames = {}
 
-def server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((SERVER_HOST, SERVER_PORT))
-    server_socket.listen()
+        self.SERVER_HOST = 'localhost'
+        self.SERVER_PORT = port
 
-    print(f"[+] Server started and listening for connetions at port {SERVER_PORT}...")
+    def run(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.SERVER_HOST, self.SERVER_PORT))
+        server_socket.listen()
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        clients.append(client_socket)
+        print(f"[+] Server started and listening for connetions at port {self.SERVER_PORT}...")
 
-        thread = threading.Thread(target=client_thread, args=(client_socket, client_address, clients, usernames))
-        thread.daemon = True
-        thread.start()
+        while True:
+            client_socket, client_address = server_socket.accept()
+            self.clients.append(client_socket)
 
-    server_socket.close()
+            thread = threading.Thread(target=self.client_thread, args=(client_socket, client_address))
+            thread.daemon = True
+            thread.start()
 
-def client_thread(client_socket, client_address, clients, usernames):
-    username = client_socket.recv(1024).decode()
-    usernames[client_socket] = username
+        server_socket.close()
 
-    print(f"[+] User \"{username}\" connected at {client_address}")
+    def client_thread(self, client_socket, client_address):
+        username = client_socket.recv(1024).decode()
+        self.usernames[client_socket] = username
 
-    for client in clients:
-        if client is not client_socket:
-            client.sendall(f"\nServer > ({username}) has connected\n\n".encode())
+        print(f"[+] User \"{username}\" connected at {client_address}")
 
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
+        for client in self.clients:
+            if client is not client_socket:
+                client.sendall(f"\nServer > ({username}) has connected\n\n".encode())
 
-            if not message:
+        while True:
+            try:
+                message = client_socket.recv(1024).decode()
+
+                if not message:
+                    break
+
+                for client in self.clients:
+                    if client is not client_socket:
+                        client.sendall(message.encode())
+            except:
                 break
 
-            for client in clients:
-                if client is not client_socket:
-                    client.sendall(message.encode())
-        except:
-            break
-
 if __name__ == "__main__":
-    server()
+    Server().run()
  
