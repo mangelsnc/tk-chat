5a6065a4eb4cb3725e8142588eaf6a8f3c15f44a
Miguel Angel Sanchez Chordi <mangel.snc@gmail.com>
2024-01-21 19:44:26 +0100

    Refactor to OOP and add new features

--- client.py
+++ client.py
@@ -5,63 +5,86 @@ import threading
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 
-def client(server_host, server_port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_host, server_port))
+class Client:
 
-    print(f"[+] Starting client")
-    
-    username = input(f"[?] Username: ")
-    client_socket.sendall(username.encode())
+    def __init__(self, server_host, server_port) -> None:
+        self.SERVER_HOST = server_host
+        self.SERVER_PORT = server_port
 
-    window = Tk()
-    window.title("Chat")
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.SERVER_HOST, self.SERVER_PORT))
 
-    text_widget = ScrolledText(window, state='disabled')
-    text_widget.pack(padx=5, pady=5)
+        print(f"[+] Starting client")
+
+        self.username = input(f"[?] Username: ")
+        self.client_socket.sendall(self.username.encode())
+
+        self.__render_chat()
+
+    def __render_chat(self):
+        self.window = Tk()
+        self.window.title("Chat")
+
+        self.text_widget = ScrolledText(self.window, state='disabled')
+        self.text_widget.pack(padx=5, pady=5)
+
+        frame_widget = Frame(self.window)
+        frame_widget.pack(fill=BOTH)
+
+        self.input_widget = Entry(frame_widget, font=("Arial", 14))
+        self.input_widget.bind("<Return>", lambda _: self.__send_message())
+        self.input_widget.pack(padx=5, pady=5, side=LEFT, fill=BOTH, expand=1)
+
+        send_widget = Button(frame_widget, text="Send", command=lambda: self.__send_message())
+        send_widget.pack(padx=3, pady=5, side=RIGHT)
+
+        list_users_widget = Button(self.window, text="List users", command=lambda: self.__list_users())
+        list_users_widget.pack(padx=5, pady=5, fill=X, expand=1)
+
+        exit_widget = Button(self.window, text="Exit", command=lambda: self.__exit())
+        exit_widget.pack(padx=5, pady=5, fill=X, expand=1)
+
+        thread = threading.Thread(target=self.__receive_message)
+        thread.daemon = True
+        thread.start()
+
+        self.window.mainloop()
+        self.client_socket.close()
+
+    def __send_message(self):
+        message = f"({self.username}) > {self.input_widget.get()}\n"
+
+        self.input_widget.delete(0, END)
+        self.text_widget.configure(state='normal')
+        self.text_widget.insert(END, message)
+        self.text_widget.configure(state='disabled')
+
+        self.client_socket.sendall(message.encode())
+
+    def __receive_message(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
  
-    frame_widget = Frame(window)
-    frame_widget.pack(fill=BOTH)
+                if not message:
+                    break
 
-    input_widget = Entry(frame_widget)
-    input_widget.bind("<Return>", lambda _: send_message(client_socket, username, text_widget, input_widget))
-    input_widget.pack(padx=5, pady=5, side=LEFT, fill=BOTH, expand=1)
+                self.text_widget.configure(state='normal')
+                self.text_widget.insert(END, message)
+                self.text_widget.configure(state='disabled')
 
-    button_widget = Button(frame_widget, text="Send")
-    button_widget.pack(padx=3, pady=5, side=RIGHT)
-
-    thread = threading.Thread(target=receive_message, args=(client_socket, text_widget))
-    thread.daemon = True
-    thread.start()
-
-    window.mainloop()
-    client_socket.close()
-
-def send_message(client_socket, username, text_widget, input_widget):
-    message = f"({username}) > {input_widget.get()}\n"
-    
-    input_widget.delete(0, END)
-    text_widget.configure(state='normal')
-    text_widget.insert(END, message)
-    text_widget.configure(state='disabled')
-
-    client_socket.sendall(message.encode())
-
-def receive_message(client_socket, text_widget):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            
-            if not message:
+            except:
+                print(f"[!] Connection lost")
                 break
 
-            text_widget.configure(state='normal')
-            text_widget.insert(END, message)
-            text_widget.configure(state='disabled')
+    def __list_users(self):
+        self.client_socket.sendall("!users".encode())
 
-        except:
-            print(f"[!] Connection lost")
-            break
+    def __exit(self):
+        self.client_socket.sendall(f"\n[!] {self.username} leave the chat\n\n".encode())
+        self.client_socket.close()
+        self.window.quit()
+        self.window.destroy()
 
 if __name__ == "__main__":
-    client("localhost", 12345)
+    Client("localhost", 12345)
